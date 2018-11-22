7779d10e76f8de827c3bdbb48cb6d1384ff5ab92
MayBMore <dh6927@naver.com>
2018-11-22 15:50:13 +0900

    sub04_1

--- main.c
+++ main.c
@@ -4,21 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	FILE*fp;
-	char input[100];
-	int i;
 	
-	fp = fopen("sample.txt", "w");
+	FILE *fp;
+	char input;
 	
-	for (i=0 ; i<3 ; i++) {
-		printf("input a word : ");
-		scanf("%s", input);
-		fprintf(fp, "%s\n", input);
+	fp = fopen("sample.txt", "r");
+	
+	while ((input = fgetc(fp)) != EOF) {
+		putchar(input);
 	}
-	//Sookmyung
-	//Electronics
-	//Programming
 	
-	fclose(fp);
 	return 0;
 }
