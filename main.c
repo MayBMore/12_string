84c5f4a53aa09aa446a795a2712fd28cad1c7c16
MayBMore <dh6927@naver.com>
2018-11-22 15:34:07 +0900

    sub03

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> //라이브러리 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	char src[100] = "Programming course";
-	int i = 0; //문자 수를 세는 변수 
+	FILE*fp;
+	char input[100];
+	int i;
 	
-	while ( src[i] != 0) {//src 내용을 dst로 옮김
-		i++;
- }
-		
-	printf("문자열\"%s\"의 길이 : %i", src, i) ;
-	printf("\n");
-	printf("%i", strlen(src)); //라이브러리에 포함. 길이세기 
+	fp = fopen("sample.txt", "w");
 	
-	//strcpy(dst, src);라이브러리에 포함. 카피하는거 
+	for (i=0 ; i<3 ; i++) {
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp, "%s\n", input);
+	}
+	//Sookmyung
+	//Electronics
+	//Programming
+	
+	fclose(fp);
 	return 0;
 }
